218d3217dde1801ccb7829b111f05c73329aed4e
justknife <hanter.mix@yandex.ru>
2021-04-02 15:17:03 +0300

    v 0.0.1.1 added key from specific operating system

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import re
 
 
-
 def checkOS():
     """ This finction need from checked OS type and use specific key package manager"""
     fin = open("/etc/os-release", "rt")
@@ -17,6 +16,7 @@ def checkOS():
 
 
 def wordpress_docker():
+    """MAIN FUNCTION"""
 
     # Package manager
     keyOS = {
@@ -50,62 +50,90 @@ def wordpress_docker():
         "CentOS Linux": "remove -y",
         "Red Hat Enterprise Linux Server": "remove -y"
     }
-    """MAIN FUNCTION"""
+
+
     key_pm = keyOS[checkOS()]
     key_pm_install = installKey[checkOS()]
     key_pm_update = updateKey[checkOS()]
     key_pm_remove = removeKey[checkOS()]
-    # install curl and git
 
-    os.system(f" sudo {key_pm} {key_pm_update}")
-    os.system(f" sudo {key_pm} {key_pm_install} curl git ")
+    # Install curl and git
+    try:
+        os.system(f" sudo {key_pm} {key_pm_update}")
+        os.system(f" sudo {key_pm} {key_pm_install} curl git ")
+    except OSError:
+        print('Error base installation. Please check your internet connection and try again')
 
-    # remove old version docker
-    os.system(f'sudo {key_pm} {key_pm_remove} docker docker-engine docker.io containerd runc')
+    if checkOS() == 'Arch Linux':
 
-    # update package
-    os.system(f" sudo {key_pm} {key_pm_update} ")
-    os.system(f"sudo {key_pm} {key_pm_install} \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release")
+        os.system(f'{key_pm} {key_pm_install} docker')
 
-    # download key and add key to apt
-    os.system('curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor '
-              '-o /usr/share/keyrings/docker-archive-keyring.gpg')
+    else:
 
-    # add package and repo to apt
-    os.system('echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
+        if checkOS() == ('Debian GNU/Linux' or 'Ubuntu GNU/Linux'):
 
-    # install docker
-    os.system(f'sudo {key_pm} {key_pm_update} &&'
-              f'sudo {key_pm} {key_pm_install} docker-ce docker-ce-cli containerd.io')
+            # remove old version docker
+            os.system(f'sudo {key_pm} {key_pm_remove} docker docker-engine docker.io containerd runc')
+        else:
+            os.system(f'sudo {key_pm} {key_pm_remove} docker \\'
+                      f'docker-client \\'
+                      f'docker-client-latest \\'
+                      f'docker-common \\'
+                      f'docker-latest \\'
+                      f'docker-latest-logrotate \\'
+                      f'docker-logrotate \\'
+                      f'docker-engine')
 
-    # download docker-compose file
+        # Update package
+        os.system(f" sudo {key_pm} {key_pm_update} ")
+
+        if checkOS() == ('Debian GNU/Linux' or 'Ubuntu GNU/Linux'):
+
+            # Install dependencies docker
+            os.system(f"sudo {key_pm} {key_pm_install} \
+                 apt-transport-https \\"
+                      f"ca-certificates \\"
+                      f"gnupg \\"
+                      f"lsb-release")
+            # Download key and add key to apt
+            os.system('curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor '
+                      '-o /usr/share/keyrings/docker-archive-keyring.gpg')
+
+            # Add package and repo to apt
+            os.system('echo \
+          "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
+
+        elif checkOS() == ('Red Hat Enterprise Linux Server' or 'CentOS Linux'):
+
+            os.system(f'sudo {key_pm} {key_pm_install} yum-utils')
+            os.system('sudo yum-config-manager \\'
+                      '--add-repo \\'
+                      'https://download.docker.com/linux/centos/docker-ce.repo')
+
+        # Install docker
+        os.system(f'sudo {key_pm} {key_pm_update} &&'
+                  f'sudo {key_pm} {key_pm_install} docker-ce docker-ce-cli containerd.io')
+
+        # Enable and start daemon docker
+        os.system('systemctl start docker && systemctl enable docker')
+
+        # Enable current user to docker user no sudo(from secure)
+        os.system('usermod -a -G docker $USER &&'
+                  'systemctl restart docker')
+
+    # Download docker-compose file
     os.system('sudo curl -L '
               '"https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" '
               '-o /usr/local/bin/docker-compose')
 
-    # add permision to use docker-compose
+    # Add permision to use docker-compose
     os.system('sudo chmod +x /usr/local/bin/docker-compose')
 
-    # create symlimk to /usr/bin/ to use command docker-compose in shell
+    # Create symlimk to /usr/bin/ to use command docker-compose in shell
     os.system('sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
-
-    # enable and start daemon docker
-    os.system('systemctl start docker && systemctl enable docker')
-
-    # enable current user to docker user no sudo(from secure)
-    os.system('usermod -a -G docker $USER &&'
-              'systemctl restart docker')
-    os.system()
 
 
 # If this file main run main function as wordpress_docker()
 if __name__ == '__main__':
     wordpress_docker()
-
